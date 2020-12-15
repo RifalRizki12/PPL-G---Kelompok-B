bf2c57f953c4f931983327739b9cad1456d16de7
Muhammad Yusuf Azizi <54919701+kproyusuf@users.noreply.github.com>
2020-12-15 14:02:16 +0700

    dah ganti laravel

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host="localhost";
-    $username="root";
-    $password="";
-    $db="framework_project";
-
-    $conn= mysqli_connect($host, $username, $password, $db) or die ('not connected');
- ?>
